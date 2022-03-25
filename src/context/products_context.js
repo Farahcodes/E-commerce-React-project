8f36b9a173d3744982da3e2f4cee5fcdcbab9491
farah <farah.fournat@gmail.com>
2022-03-25 20:29:46 +0100

    completed fetching data

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -14,7 +14,11 @@ import {
 } from '../actions'
 
 const initialState = {
-  isSidebarOpen:false
+  isSidebarOpen:false,
+  products_loading:false,
+  products_error:false,
+  products:[],
+  featured_products:[],
 }
 
 const ProductsContext = React.createContext()
@@ -30,7 +34,15 @@ export const ProductsProvider = ({ children }) => {
   }
 
   const fetchProducts = async(url) =>{
-    const response = await axios.get(url)
+    dispatch({type:GET_PRODUCTS_BEGIN})
+    try {
+      const response = await axios.get(url)
+      const products = response.data
+      dispatch({type:GET_PRODUCTS_SUCCESS,payload:products})
+    } catch (error) {
+      dispatch({type:GET_PRODUCTS_ERROR})
+    }
+    
   }
 
   useEffect(()=>{
