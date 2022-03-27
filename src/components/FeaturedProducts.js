a8aacff4141141866d77dbfba1efc5508e9f4e9a
farah <farah.fournat@gmail.com>
2022-03-27 15:00:33 +0200

    loading page

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -7,6 +7,11 @@ import Loading from './Loading'
 import Product from './Product'
 
 const FeaturedProducts = () => {
+  const{products_loading:loading,products_error:error,featured_products:featured}=useProductsContext();
+
+  if(loading){
+    return <Loading/>
+  }
   return <h4>featured products</h4>
 }
 
