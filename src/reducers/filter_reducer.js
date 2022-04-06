1ab2ec0a4ebb50a8dee8c63333c023baa3c265a8
farah <farah.fournat@gmail.com>
2022-04-06 11:15:11 +0200

    added functionality for price filter

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -77,6 +77,7 @@ const filter_reducer = (state, action) => {
       })
     }
     //price
+    tempProducts = tempProducts.filter((product)=>product.price <= price)
 
     //shipping
     if(shipping){
