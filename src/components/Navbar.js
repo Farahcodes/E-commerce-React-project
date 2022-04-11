23a94697c5e46fe32edc8757cd98c520a85caceb
farah <farah.fournat@gmail.com>
2022-04-11 12:38:23 +0200

    hide checkout in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,34 +7,37 @@ import { links } from '../utils/constants'
 import CartButtons from './CartButtons'
 import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
-
 const Nav = () => {
-  const {openSidebar} = useProductsContext()
+  const { openSidebar } = useProductsContext()
+  const { myUser } = useUserContext()
   return (
     <NavContainer>
-      <div className="nav-center">
-        <div className="nav-header">
+      <div className='nav-center'>
+        <div className='nav-header'>
           <Link to='/'>
-            <img src={logo} alt="comfy baby" />
+            <img src={logo} alt='comfy sloth' />
           </Link>
           <button type='button' className='nav-toggle' onClick={openSidebar}>
-            <FaBars/>
+            <FaBars />
           </button>
         </div>
-
-          <ul className='nav-links'>
-            {links.map((link) => {
-              const { id, text, url } = link
-              return (
-                <li key={id}>
-                  <Link to={url}>{text}</Link>
-                </li>
-              )
-            })}
-          </ul>
-          <CartButtons/>
-        </div>
-    
+        <ul className='nav-links'>
+          {links.map((link) => {
+            const { id, text, url } = link
+            return (
+              <li key={id}>
+                <Link to={url}>{text}</Link>
+              </li>
+            )
+          })}
+          {myUser && (
+            <li>
+              <Link to='/checkout'>checkout</Link>
+            </li>
+          )}
+        </ul>
+        <CartButtons />
+      </div>
     </NavContainer>
   )
 }
@@ -44,7 +47,6 @@ const NavContainer = styled.nav`
   display: flex;
   align-items: center;
   justify-content: center;
-
   .nav-center {
     width: 90vw;
     margin: 0 auto;
