be7ed6201ce5cd3fc15c2622b493f35620d96042
farah <farah.fournat@gmail.com>
2022-04-11 14:15:35 +0200

    updated private route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -5,10 +5,10 @@ import { useUserContext } from '../context/user_context';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const PrivateRoute = ({ children,...rest }) => {
-  const {myUser} = useUserContext()
+  const {user} = useAuth0()
   return(
     <Route {...rest} render={()=>{
-      return myUser ? children : <Redirect to='/'></Redirect>
+      return user ? children : <Redirect to='/'></Redirect>
     }}>
 
     </Route>
