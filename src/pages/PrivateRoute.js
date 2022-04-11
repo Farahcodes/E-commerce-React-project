f3bdc051dc38310da9a959c242b632f279824651
farah <farah.fournat@gmail.com>
2022-04-11 13:09:53 +0200

    corrected Private Route

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,13 +1,18 @@
 
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
+import { useUserContext } from '../context/user_context';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const PrivateRoute = ({ children }) => {
-  const { user } = useAuth0();
-  if (!user) {
-    return <Navigate to='/' />;
-  }
-  return children;
+const PrivateRoute = ({ children,...rest }) => {
+  const {myUser} = useUserContext()
+  return(
+    <Route {...rest} render={()=>{
+      return myUser ? children : <Redirect to='/'></Redirect>
+    }}>
+
+    </Route>
+  )
+  
 };
 export default PrivateRoute;
